d75c233447fb45891b49d56a88bd0bb1287ce4e1
Nayan-Bhisara <bhisaranayan3196@gmail.com>
2023-12-28 13:39:03 +0530

    c

---
+++ 8_2_q3.c
@@ -0,0 +1,36 @@
+/*
+Q.3 Write a Program to find square of each element from the given array.
+For example,
+Input:
+Enter array size: 5
+
+Enter array elements:
+a[0] = 7
+a[1] = 4
+a[2] = 9
+a[3] = 5
+a[4] = 2
+
+
+Output:
+The squares are: 49, 16, 81, 25, 4
+*/
+
+
+void main(){
+	
+	int i,n;
+	int a[n];
+	
+	printf("enter size of array A:- ");
+	scanf("%d",&n);
+	
+	for(i=0;i<n; i++){
+		printf("enter value A:- ");
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++){
+		printf("%d",a[i]*a[i]);
+	}
+	
+}
